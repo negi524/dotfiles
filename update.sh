faede115b60bb7ae2e1ae493edf3b5ac9297fccd
negi <negishi.riku21@gmail.com>
2019-02-17 17:06:45 +0900

    fishのシンボリックリンク追加

--- update.sh
+++ update.sh
@@ -8,4 +8,7 @@ cd ${DOTPATH}
 ln -sf ${DOTPATH}/.vimrc ${HOME}/.vimrc
 echo "symbolic link: ${HOME}/.vimrc -> ${DOTPATH}/.vimrc"
 
+ln -sf ${DOTPATH}/.config/fish/config.fish ${HOME}/.config/fish/config.fish
+echo "symbolic link: ${HOME}/.config/fish/config.fish -> ${DOTPATH}/.config/fish/config.fish
+
 exit 0
