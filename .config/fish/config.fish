f385ea7a919e0c5dae2f670489c5f9920c1eadff
negi <negishi.riku21@gmail.com>
2019-02-17 18:33:01 +0900

    【fish】aliasと配色の設定

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,10 @@
+# 配色の設定
+export LSCOLORS=gxfxcxdxbxegedabagacad
+
+# aliasの設定
+alias ..='cd ..'
+alias gs='git status'
+
 # pyenv用の設定
 set -x PATH $HOME/.pyenv/bin $PATH
 eval (pyenv init - | source)
