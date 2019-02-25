835fdf824dfdfdf6ab75608a3fd6ef2d855de9cd
negi <negishi.riku21@gmail.com>
2019-02-25 22:40:42 +0900

    fishの設定ディレクトリの存在確認を行う

--- setup.sh
+++ setup.sh
@@ -8,10 +8,15 @@ cd ${DOTPATH}
 ln -sf ${DOTPATH}/.vimrc ${HOME}/.vimrc
 echo "symbolic link: ${HOME}/.vimrc -> ${DOTPATH}/.vimrc"
 
-ln -sf ${DOTPATH}/.config/fish/config.fish ${HOME}/.config/fish/config.fish
-echo "symbolic link: ${HOME}/.config/fish/config.fish -> ${DOTPATH}/.config/fish/config.fish"
-
 ln -sf ${DOTPATH}/.bash_aliases ${HOME}/.bash_aliases
 echo "symbolic link: ${HOME}/.bash_aliases -> ${DOTPATH}/.bash_aliases"
+
+# fishの設定ディレクトリがある場合のみシンボリックリンクを作成する
+if [ -d ${HOME}/.config/fish ]; then
+  ln -sf ${DOTPATH}/.config/fish/config.fish ${HOME}/.config/fish/config.fish
+  echo "symbolic link: ${HOME}/.config/fish/config.fish -> ${DOTPATH}/.config/fish/config.fish"
+else
+  echo "No such directory: ${HOME}/.cofig/fish"
+fi
 
 exit 0
