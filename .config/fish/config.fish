24f88d2d557bab20f4e91731b7bee6d1cfc494c3
negi <negishi.riku21@gmail.com>
2019-03-09 12:25:56 +0900

    pythonのパス設定をコメントアウト

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,5 +6,5 @@ alias ..='cd ..'
 alias gs='git status'
 
 # pyenv用の設定
-set -x PATH $HOME/.pyenv/bin $PATH
-eval (pyenv init - | source)
+# set -x PATH $HOME/.pyenv/bin $PATH
+# eval (pyenv init - | source)
