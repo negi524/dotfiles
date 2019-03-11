c58ef846e98c645a42c23c71a893a9c87652f471
negi <negishi.riku21@gmail.com>
2019-03-11 23:54:33 +0900

    関数化

--- setup.sh
+++ setup.sh
@@ -5,11 +5,19 @@ DOTPATH="${HOME}/dotfiles"
 
 cd ${DOTPATH}
 
-ln -sf ${DOTPATH}/.vimrc ${HOME}/.vimrc
-echo "symbolic link: ${HOME}/.vimrc -> ${DOTPATH}/.vimrc"
 
-ln -sf ${DOTPATH}/.bash_profile ${HOME}/.bash_profile
-echo "symbolic link: ${HOME}/.bash_profile -> ${DOTPATH}/.bash_profile"
+# シンボリックリンクを貼る関数
+# $1: 対象ファイル
+create_ln () {
+  local FILE=$1
+
+  ln -sf ${DOTPATH}/${FILE} ${HOME}/${FILE}
+  echo "symbolic link: ${HOME}/${FILE} -> ${DOTPATH}/${FILE}"
+  return 0
+}
+
+create_ln ".vimrc"
+create_ln ".bash_profile"
 
 # vimの設定ディレクトリが存在しない場合は作成する
 if [ ! -d ${HOME}/.vim/colors ]; then
@@ -19,13 +27,11 @@ elif [ ! -d ${HOME}/.vim ]; then
   mkdir ${HOME}/.vim/colors
 fi
 
-ln -sf ${DOTPATH}/.vim/colors/hybrid.vim ${HOME}/.vim/colors/hybrid.vim
-echo "symbolic link: ${HOME}/.vim/colors/hybrid.vim -> ${DOTPATH}/.vim/colors/hybrid.vim"
+create_ln ".vim/colors/hybrid.vim"
 
 # fishの設定ディレクトリがある場合のみシンボリックリンクを作成する
 if [ -d ${HOME}/.config/fish ]; then
-  ln -sf ${DOTPATH}/.config/fish/config.fish ${HOME}/.config/fish/config.fish
-  echo "symbolic link: ${HOME}/.config/fish/config.fish -> ${DOTPATH}/.config/fish/config.fish"
+  create_ln ".config/fish/config.fish"
 else
   echo "No such directory: ${HOME}/.cofig/fish"
 fi
