218c7921fe5bd992db549786f9ce7f383ba93b00
negi <negishi.riku21@gmail.com>
2019-03-12 01:38:15 +0900

    if文追加

--- init.sh
+++ init.sh
@@ -9,6 +9,9 @@ cd ${DOTPATH}
 if [ ! -d ${DOTPATH}/downloads/vim-hybrid ]; then
   git clone git@github.com:w0ng/vim-hybrid.git ${DOTPATH}/downloads/vim-hybrid
 fi
-cp ${DOTPATH}/downloads/vim-hybrid/colors/hybrid.vim ${DOTPATH}/.vim/colors/
+
+if [ ! -f ${DOTPATH}/.vim/colors/hybrid.vim ]; then
+  cp ${DOTPATH}/downloads/vim-hybrid/colors/hybrid.vim ${DOTPATH}/.vim/colors/
+fi
 
 exit 0
