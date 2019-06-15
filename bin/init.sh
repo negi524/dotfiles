536dbcb3c68eee2bd2c549206f272d1b40dbdafa
negi524 <egtnrumpbiizarre.info@gmail.com>
2019-06-15 09:49:48 +0900

    main関数追加

--- bin/init.sh
+++ bin/init.sh
@@ -5,14 +5,19 @@ DOTPATH="${HOME}/dotfiles"
 
 cd ${DOTPATH}
 
-# hybridのデータをダウンロードする
-if [ ! -d ${DOTPATH}/downloads/vim-hybrid-master ]; then
-  wget https://github.com/w0ng/vim-hybrid/archive/master.zip -P ${DOTPATH}/downloads/
-  unzip ${DOTPATH}/downloads/master.zip -d ${DOTPATH}/downloads/
-fi
+# main関数
+function main () {
+  # hybridのデータをダウンロードする
+  if [ ! -d ${DOTPATH}/downloads/vim-hybrid-master ]; then
+    wget https://github.com/w0ng/vim-hybrid/archive/master.zip -P ${DOTPATH}/downloads/
+    unzip ${DOTPATH}/downloads/master.zip -d ${DOTPATH}/downloads/
+  fi
 
-if [ ! -f ${DOTPATH}/.vim/colors/hybrid.vim ]; then
-  cp ${DOTPATH}/downloads/vim-hybrid-master/colors/hybrid.vim ${DOTPATH}/.vim/colors/
-fi
+  if [ ! -f ${DOTPATH}/.vim/colors/hybrid.vim ]; then
+    cp ${DOTPATH}/downloads/vim-hybrid-master/colors/hybrid.vim ${DOTPATH}/.vim/colors/
+  fi
+}
+
+main
 
 exit 0
--- bin/setup.sh
+++ bin/setup.sh
@@ -5,50 +5,55 @@ DOTPATH="${HOME}/dotfiles"
 
 cd ${DOTPATH}
 
+# main関数
+function main () {
+  create_ln ".vimrc"
+  create_ln ".bash_profile"
+
+  # vim --------------------------------------
+
+  # vimの設定ディレクトリが存在しない場合は作成する
+  if [ ! -d ${HOME}/.vim ]; then
+    mkdir ${HOME}/.vim
+  fi
+
+  if [ ! -d ${HOME}/.vim/colors ]; then
+    mkdir ${HOME}/.vim/colors
+  fi
+
+  create_ln ".vim/colors/hybrid.vim"
+
+  if [ ! -d ${HOME}/.vim/autoload ]; then
+    mkdir ${HOME}/.vim/autoload
+  fi
+  create_ln ".vim/autoload/plug.vim"
+
+  # fish -------------------------------------
+
+  # fishの設定ディレクトリが存在しない場合は作成する
+  if [ ! -d ${HOME}/.config/fish ]; then
+    mkdir ${HOME}/.config/fish
+  fi
+
+  if [ ! -d ${HOME}/.config/fish/functions ]; then
+    mkdir ${HOME}/.config/fish/functions
+  fi
+
+  create_ln ".config/fish/config.fish"
+  create_ln ".config/fish/functions/fish_prompt.fish"
+}
 
 # シンボリックリンクを貼る関数
 # $1: 対象ファイル
-create_ln () {
+function create_ln () {
   local FILE=$1
 
-  ln -sf ${DOTPATH}/${FILE} ${HOME}/${FILE}
-  echo "symbolic link: ${HOME}/${FILE} -> ${DOTPATH}/${FILE}"
+  ln -vsf ${DOTPATH}/${FILE} ${HOME}/${FILE}
   return 0
 }
 
-create_ln ".vimrc"
-create_ln ".bash_profile"
+# main関数実行
+main
 
-# vim --------------------------------------
-
-# vimの設定ディレクトリが存在しない場合は作成する
-if [ ! -d ${HOME}/.vim ]; then
-  mkdir ${HOME}/.vim
-fi
-
-if [ ! -d ${HOME}/.vim/colors ]; then
-  mkdir ${HOME}/.vim/colors
-fi
-
-create_ln ".vim/colors/hybrid.vim"
-
-if [ ! -d ${HOME}/.vim/autoload ]; then
-  mkdir ${HOME}/.vim/autoload
-fi
-create_ln ".vim/autoload/plug.vim"
-
-# fish -------------------------------------
-
-# fishの設定ディレクトリが存在しない場合は作成する
-if [ ! -d ${HOME}/.config/fish ]; then
-  mkdir ${HOME}/.config/fish
-fi
-
-if [ ! -d ${HOME}/.config/fish/functions ]; then
-  mkdir ${HOME}/.config/fish/functions
-fi
-
-create_ln ".config/fish/config.fish"
-create_ln ".config/fish/functions/fish_prompt.fish"
 
 exit 0
