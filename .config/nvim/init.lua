4bd48b49e2bbdcc4ad0a27f29031d9b275e78697
negi524 <egtnrumpbiizarre.info@gmail.com>
2023-04-27 09:35:30 +0900

    基本設定追加

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,21 @@
--- set runtimepath^=~/.vim runtimepath+=~/.vim/after
--- let &packpath = &runtimepath
--- source ~/.vimrc
-print('hello, lua')
+-- TODO: プラグイン設定
+
+vim.opt.number = true                             -- 行番号を表示
+vim.opt.title = true                              -- ファイル名を表示
+vim.opt.smartcase = true                          -- 小文字で検索時、大文字小文字を無視する
+vim.opt.wrap = false                              -- 行の折返しを無効にする
+
+-- 括弧
+vim.opt.showmatch = true                          -- 括弧入力時の対応する括弧を表示
+vim.opt.matchtime = 1                             -- 対応括弧に飛ぶ時間を0.1×1秒に設定
+
+-- タブ、インデント
+vim.opt.tabstop = 2                               -- タブの文字幅
+vim.opt.shiftwidth = 2                            -- 自動インデントの幅
+vim.opt.smartindent = true                        -- 一つ前の行に基づくインデントを有効
+vim.opt.expandtab = true                          -- タブ入力を空白にする
+vim.opt.list = true                               -- listオプションを有効にする
+-- vim.cmd([[set listchars=tab:»-,trail:-,nbsp:]])   -- listオプションを設定する
+
+-- ハイライト
+vim.opt.hlsearch = true               -- 検索ワードのハイライトを行う
