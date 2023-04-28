d4bbfbc4bbb281a6419a2fbbd9e3516ac782a5cb
negi524 <egtnrumpbiizarre.info@gmail.com>
2023-04-28 12:15:48 +0900

    Neovimの設定ファイルをluaに移行する (#54)
    * init.vim→init.luaを作成し、packer.nvimをインストール
    
    * プラグイン記載用のファイル作成
    
    * *.luaファイルもシンボリックリンクで同期するように設定
    
    * 基本設定追加
    
    * タブと行末空白を可視化する設定
    
    * プレビューウィンドウを垂直分割して表示する
    
    * プラグイン設定

---
+++ .config/nvim/init.lua
@@ -0,0 +1,29 @@
+-- TODO: プラグイン設定
+
+vim.opt.number = true                             -- 行番号を表示
+vim.opt.title = true                              -- ファイル名を表示
+vim.opt.smartcase = true                          -- 小文字で検索時、大文字小文字を無視する
+vim.opt.wrap = false                              -- 行の折返しを無効にする
+
+-- 括弧
+vim.opt.showmatch = true                          -- 括弧入力時の対応する括弧を表示
+vim.opt.matchtime = 1                             -- 対応括弧に飛ぶ時間を0.1×1秒に設定
+
+-- タブ、インデント
+vim.opt.tabstop = 2                               -- タブの文字幅
+vim.opt.shiftwidth = 2                            -- 自動インデントの幅
+vim.opt.smartindent = true                        -- 一つ前の行に基づくインデントを有効
+vim.opt.expandtab = true                          -- タブ入力を空白にする
+vim.opt.list = true                               -- listオプションを有効にする
+vim.opt.listchars = { tab = '»-' , trail = '-' }  -- タブと行末空白を可視化する
+
+-- ハイライト
+vim.opt.hlsearch = true                           -- 検索ワードのハイライトを行う
+
+-- netrw
+vim.g.netrw_preview = 1                           -- プレビューウィンドウを垂直分割で表示する
+
+-- プラグイン
+require('plugins')
+
+vim.opt.helplang = 'ja,en'                        -- ヘルプの言語を日本語優先にする
--- etc/neovim_setting.sh
+++ etc/neovim_setting.sh
@@ -3,15 +3,16 @@ function neovim_setting () {
   log "Neovimの設定反映"
 
   # neovimの設定ディレクトリが存在しない場合は作成する
-  local NVIM_DIRS=(".config/nvim" ".vim/autoload")
+  local NVIM_DIRS=(".config/nvim" ".vim/autoload" ".config/nvim/lua")
   for directory in ${NVIM_DIRS[@]}
   do
     set_dir ${directory}
   done
 
   # シンボリックリンクを作成
-  local NVIM_FILES=(".config/nvim/init.vim")
+  local NVIM_FILES=(".config/nvim/init.lua")
   NVIM_FILES+=(".config/nvim/*.vim")
+  NVIM_FILES+=(".config/nvim/**/*.lua")
   for file in ${NVIM_FILES[@]}
   do
     create_ln ${file}
