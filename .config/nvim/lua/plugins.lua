e6321aac1b6f19d506b51597baec968bd88d2a0a
negi524 <egtnrumpbiizarre.info@gmail.com>
2023-04-28 15:33:49 +0900

    gitの差分を表示するように修正

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -3,4 +3,14 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'vim-jp/vimdoc-ja'
+  use 'tpope/vim-fugitive'
+  use {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup {
+        signcolumn = number,
+        numhl= true,
+      }
+    end
+  }
 end)
