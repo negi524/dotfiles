78df9e11a1364fc6fe9082485e6e5eb55ca20134
negi524 <egtnrumpbiizarre.info@gmail.com>
2023-04-28 13:09:47 +0900

    カラースキームの導入

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,4 @@
--- TODO: プラグイン設定
-
+-- 基本設定
 vim.opt.number = true                             -- 行番号を表示
 vim.opt.title = true                              -- ファイル名を表示
 vim.opt.smartcase = true                          -- 小文字で検索時、大文字小文字を無視する
@@ -22,6 +21,54 @@ vim.opt.hlsearch = true                           -- 検索ワードのハイラ
 
 -- netrw
 vim.g.netrw_preview = 1                           -- プレビューウィンドウを垂直分割で表示する
+
+
+-- カラースキーム
+
+-- 対応括弧の色
+vim.api.nvim_create_autocmd({"ColorScheme"},{
+  pattern = {"*"},
+  command = "highlight MatchParen ctermfg=0 ctermbg=21 guifg=Black guibg=Blue1",
+})
+
+-- 行番号の色
+vim.api.nvim_create_autocmd({"ColorScheme"},{
+  pattern = {"*"},
+  command = "highlight LineNr ctermfg=lightmagenta",
+})
+
+-- コメントの色
+vim.api.nvim_create_autocmd({"ColorScheme"},{
+  pattern = {"*"},
+  command = "highlight Comment ctermfg=243",
+})
+
+-- 通常の文字色
+vim.api.nvim_create_autocmd({"ColorScheme"},{
+  pattern = {"*"},
+  command = "highlight Normal ctermfg=248",
+})
+
+-- ポップアップメニュー(通常の項目)
+vim.api.nvim_create_autocmd({"ColorScheme"},{
+  pattern = {"*"},
+  command = "highlight Pmenu ctermfg=232 ctermbg=255",
+})
+
+-- ポップアップメニュー(選択中の項目)
+vim.api.nvim_create_autocmd({"ColorScheme"},{
+  pattern = {"*"},
+  command = "highlight PmenuSel ctermfg=41 ctermbg=54",
+})
+
+-- ビジュアルモード選択
+vim.api.nvim_create_autocmd({"ColorScheme"},{
+  pattern = {"*"},
+  command = "highlight Visual ctermbg=239",
+})
+
+vim.g.hybrid_custom_term_colors = 1               -- iTerm2用のhybrid設定
+vim.cmd("colorscheme hybrid")
 
 -- プラグイン
 require('plugins')
