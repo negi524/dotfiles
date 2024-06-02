70470f5826967ca394c38b97169fc55ab4eb17ef
negi524 <egtnrumpbiizarre.info@gmail.com>
2024-06-02 16:52:29 +0900

    feat: 不要なシンボリックリンク削除

--- etc/neovim_setting.sh
+++ etc/neovim_setting.sh
@@ -11,7 +11,6 @@ function neovim_setting () {
 
   # シンボリックリンクを作成
   local NVIM_FILES=(".config/nvim/init.lua")
-  NVIM_FILES+=(".config/nvim/**/*.lua")
   NVIM_FILES+=(".config/nvim/lua/plugins")
   for file in ${NVIM_FILES[@]}
   do
