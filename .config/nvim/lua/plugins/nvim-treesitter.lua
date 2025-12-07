01a65f5d60f285262c00421dae30124b821cdd5c
negi524 <egtnrumpbiizarre.info@gmail.com>
2025-12-08 08:59:08 +0900

    feat: nvim-treesitterの設定修正 (#101)

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,7 +1,29 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  lazy = false,
   opts = {
-        ensure_installed = { "lua", "typescript", "vim", "markdown", "bash", "java", "ruby", "tsx", "vue", "json", "yaml", "python" },
-        sync_install = true,
+    ensure_installed = {
+      "lua",
+      "typescript",
+      "javascript",
+      "vim",
+      "markdown",
+      "bash",
+      "java",
+      "ruby",
+      "tsx",
+      "vue",
+      "json",
+      "yaml",
+      "python",
+      "astro",
+      "html",
+      "css"
+    },
+    sync_install = true,
+    highlight = {
+      enable = true,
+    }
     },
 }
