beb858a73a84898e262bc1330fcff2512585c80c
negi524 <egtnrumpbiizarre.info@gmail.com>
2025-12-08 22:03:30 +0900

    telescope.nvim設定 (#102)
    * feat: install telescope
    
    * feat: コマンド調整
    
    * feat: add dockerfile filetype

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,31 @@
+return {
+  'nvim-telescope/telescope.nvim',
+  tag = 'v0.2.0',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  keys = {
+    { "<leader>ff", "<cmd>Telescope find_files hidden=true<cr>", desc = "Find files" },
+    { "<leader>fg", "<cmd>Telescope live_grep<cr>",  desc = "Live grep" },
+    { "<leader>fb", "<cmd>Telescope buffers<cr>",    desc = "Buffers" },
+    { "<leader>fh", "<cmd>Telescope help_tags<cr>",  desc = "Help" },
+  },
+  opts = {
+    defaults = {
+      file_ignore_patterns = {
+        "^.git/",
+        "^node_modules/",
+        "^build/",
+        "^dist/",
+      },
+      vimgrep_arguments = {
+        "rg",
+        "--color=never",
+        "--no-heading",
+        "--with-filename",
+        "--line-number",
+        "--column",
+        "--smart-case",
+        "--hidden",
+      }
+    }
+  }
+}
