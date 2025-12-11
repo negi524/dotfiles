04d3fc2271965ab73a0605e1e25cf2f4ba7a33d2
negi524 <egtnrumpbiizarre.info@gmail.com>
2025-12-11 16:35:27 +0900

    fix: コマンド修正

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -12,10 +12,9 @@ return {
 
       -- 基本操作
       vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
-      vim.keymap.set('n', 'o', api.node.open.edit, opts('Open'))
       vim.keymap.set('n', '<2-LeftMouse>', api.node.open.edit, opts('Open'))
       vim.keymap.set('n', '<Tab>', api.node.open.preview, opts('Preview'))
-      vim.keymap.set('n', 'za', api.tree.expand_all, opts('Expand All'))
+      vim.keymap.set('n', 'zO', api.tree.expand_all, opts('Expand All'))
       vim.keymap.set('n', 'zc', api.node.navigate.parent_close, opts('Close Directory'))
       vim.keymap.set("n", "K", api.node.show_info_popup, opts("Info"))
       vim.keymap.set("n", "t", api.node.open.tab, opts("Open: New Tab"))
