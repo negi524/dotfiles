b3864ec351f8b354f98b4e1f24a802ecb0654d8f
negi524 <egtnrumpbiizarre.info@gmail.com>
2025-12-11 21:53:26 +0900

    diffview.nvim (#106)
    * feat: 現在のブランチのPR diffを確認するコマンドを追加
    
    * feat: luaのファイルでvimをグローバル変数として認識

--- .config/nvim/lua/plugins/diffview.lua
+++ .config/nvim/lua/plugins/diffview.lua
@@ -1,3 +1,26 @@
 return {
-  'sindrets/diffview.nvim'
+  "sindrets/diffview.nvim",
+  config = function()
+    local function open_pr_diff()
+      -- 現在のブランチ名
+      local current = vim.fn.systemlist("git branch --show-current")[1]
+
+      -- GH CLI で base ブランチを取得
+      local base = vim.fn.systemlist(
+        "gh pr view --json baseRefName --jq .baseRefName 2>/dev/null"
+      )[1]
+
+      if base == nil or base == "" then
+        print("GitHub PR が見つかりません。")
+        return
+      end
+
+      -- Diffview で PR と同等の差分を表示
+      vim.cmd("DiffviewOpen " .. base .. "..." .. current)
+    end
+
+    vim.keymap.set("n", "<leader>gp", open_pr_diff, {
+      desc = "Diffview: show diff of current PR",
+    })
+  end,
 }
