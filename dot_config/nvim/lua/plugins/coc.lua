021994ac0076eec0ec1f3b1e545bf2a61aae4534
negi524 <egtnrumpbiizarre.info@gmail.com>
2026-01-06 12:30:18 +0900

    feat: Kキーでドキュメント、型情報を表示するように

--- dot_config/nvim/lua/plugins/coc.lua
+++ dot_config/nvim/lua/plugins/coc.lua
@@ -18,5 +18,18 @@ return {
     keyset("n", "<leader>gy", "<Plug>(coc-type-definition)", {silent = true})
     keyset("n", "<leader>gi", "<Plug>(coc-implementation)", {silent = true})
     keyset("n", "<leader>gr", "<Plug>(coc-references)", {silent = true})
+
+    -- Kキーでドキュメント・型情報を表示
+    function _G.show_docs()
+      local cw = vim.fn.expand('<cword>')
+      if vim.fn.index({'vim', 'help'}, vim.bo.filetype) >= 0 then
+        vim.api.nvim_command('h ' .. cw)
+      elseif vim.api.nvim_eval('coc#rpc#ready()') then
+        vim.fn.CocActionAsync('doHover')
+      else
+        vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
+      end
+    end
+    keyset("n", "K", '<CMD>lua _G.show_docs()<CR>', {silent = true})
   end
 }
