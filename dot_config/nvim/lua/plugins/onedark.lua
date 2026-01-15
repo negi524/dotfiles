c17f7376d927acba7349036f552bacee9195af6b
negi524 <egtnrumpbiizarre.info@gmail.com>
2026-01-15 21:48:05 +0900

    feat: Neovimの背景も透過させる

--- dot_config/nvim/lua/plugins/onedark.lua
+++ dot_config/nvim/lua/plugins/onedark.lua
@@ -4,7 +4,8 @@ return {
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
     require('onedark').setup {
-      style = 'dark'
+      style = 'dark',
+      transparent = true,
     }
     require('onedark').load()
   end
