3bf60b441689f978011c78efa15147110f917553
negi524 <egtnrumpbiizarre.info@gmail.com>
2026-02-28 19:36:32 +0900

    feat: 定義ジャンプコマンド実装

--- dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,4 +2,12 @@ return {
   'neovim/nvim-lspconfig',
   -- Bufferが読み込まれるときをトリガーに遅延ロードする
   -- event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    vim.api.nvim_create_autocmd('LspAttach', {
+      callback = function(args)
+        local opts = { buffer = args.buf }
+        vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, opts)
+      end,
+    })
+  end,
 }
