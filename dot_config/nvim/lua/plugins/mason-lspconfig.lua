90d5d283d3b6748c68110fe905b919b9e817f7fa
negi524 <egtnrumpbiizarre.info@gmail.com>
2026-02-28 17:47:31 +0900

    feat: add lsp of lua and python

--- dot_config/nvim/lua/plugins/mason-lspconfig.lua
+++ dot_config/nvim/lua/plugins/mason-lspconfig.lua
@@ -1,6 +1,11 @@
 return {
   "mason-org/mason-lspconfig.nvim",
-  opts = {},
+  opts = {
+    ensure_installed = {
+      "lua_ls",
+      "pylsp"
+    },
+  },
   dependencies = {
     "mason-org/mason.nvim",
     "neovim/nvim-lspconfig",
