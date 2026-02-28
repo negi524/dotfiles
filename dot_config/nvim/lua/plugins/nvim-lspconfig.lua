89e18289f6e5249b34edac2684da2acf00ab610f
negi524 <egtnrumpbiizarre.info@gmail.com>
2026-02-28 19:48:36 +0900

    feat: 遅延ロード

--- dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,9 +1,11 @@
 return {
   'neovim/nvim-lspconfig',
   -- Bufferが読み込まれるときをトリガーに遅延ロードする
-  -- event = { "BufReadPre", "BufNewFile" },
+  event = { "BufReadPre", "BufNewFile" },
   config = function()
+    --- Launguage Serverに接続したときだけ設定される
     vim.api.nvim_create_autocmd('LspAttach', {
+      desc = "Attach key mappings for LSP functionalities",
       callback = function(args)
         local opts = { buffer = args.buf }
         vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, opts)
